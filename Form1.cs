fe11be2afe24902bbda615bcbca78b85629d3dbf
Marvin <marvin.richter@smail.th-koeln.de>
2021-01-05 20:18:31 +0100

    2nd comment added

--- Form1.cs
+++ Form1.cs
@@ -29,7 +29,7 @@ namespace Snake
         int[] foodXPos;
         int[] foodYPos;
         int AmountOfFood = 1;       //Amount of food on screen at a time
-        bool rePlaceFood = true;  // comment for github test 
+        bool rePlaceFood = true;  // comment for github test 2
 
         private void Form1_Load(object sender, EventArgs e)
         {
